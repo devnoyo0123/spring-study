8277ca6be00c170b25d0cb16a75e9c072a41a221
yobs <devnoyo0123@gmail.com>
2024-04-01 15:10:04 +0900

    프로젝트 초기세팅

---
+++ src/main/java/com/example/mildangbespringstudy/MildangBeSpringStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mildangbespringstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MildangBeSpringStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MildangBeSpringStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mildangbespringstudy/MildangBeSpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mildangbespringstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MildangBeSpringStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
